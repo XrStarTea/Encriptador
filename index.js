099956d8541ff962c478c489d37348cabf54d2fd
Pipe <nombreasombrozo@gmail.com>
2024-08-20 21:24:41 -0400

    test de javascript

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function Funcion_Encriptar(){
-    let Ingreso_de_texto 
+    let texto=document.querySelector("#Ingreso_de_texto")
+    if(texto.value === ""){
+        alert("ingrese texto a encriptar")
+    }
+    for (let i = 0; i < texto.length; i++) {
+    }
+    let nuevo_texto = ""
+    nuevo_texto = texto.value.replace('a','test')
+    texto.innerHTML=""
+    texto.value=""
+    texto.value=nuevo_texto
+    texto.innerText= nuevo_texto;
+    console.log(texto);
+    
+    
 }
