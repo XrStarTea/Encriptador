520387fb2ff1562efbba1210215acc3d6b585f05
Pipe <nombreasombrozo@gmail.com>
2024-08-22 16:02:01 -0400

    version funcional

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let texto=document.querySelector("#Ingreso_de_texto");
 let esconder=document.querySelector(".bttn-copiar");
 let boton_encriptar=document.querySelector(".Boton_encriptar")
 let boton_desencriptar=document.querySelector(".Boton_desencriptar")
+let muneco = document.querySelector("#muneco")
+let mensaje = document.querySelector("#Mensaje_resultado")
+let text___=document.querySelector("#Text___")
 
 //variables
 esconder.classList.add("ocultar")
@@ -19,16 +22,27 @@ function Funcion_Desencriptar(texto){
 //creamos la funcion para desencriptar
 boton_desencriptar.addEventListener("click",(e)=>{
     let Encriptado_texto = Funcion_Desencriptar(texto.value);
+    muneco.classList.add("ocultar")
+    mensaje.classList.add("ocultar")
+    esconder.classList.add("mostrar")
+    text___.classList.add("posicion_texto")
+    text___.innerHTML= Encriptado_texto
     console.log(Encriptado_texto)
 })
 //creamos un evento que al hacer click llame a la funcion desencriptar
 boton_encriptar.addEventListener("click",(e)=>{
     let Encriptado_texto = Funcion_Encriptar(texto.value);
+    muneco.classList.add("ocultar")
+    mensaje.classList.add("ocultar")
+    esconder.classList.add("mostrar")
+    text___.classList.add("posicion_texto")
+    text___.innerHTML= Encriptado_texto
     console.log(Encriptado_texto)
+    return Encriptado_texto;
 })
 //creamos un evento que al hacer click llame a la funcion encriptar
 esconder.addEventListener("click", (e) => {
-    let texto_copiar = texto.value;
+    let texto_copiar = text___.textContent;
     navigator.clipboard
       .writeText(texto_copiar)
       .then(function () {
@@ -38,3 +52,5 @@ esconder.addEventListener("click", (e) => {
         console.error("Error al copiar al portapapeles: ", error);
       });
   });
+
+  //copiamos 
